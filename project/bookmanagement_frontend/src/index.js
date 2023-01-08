0828297685e6862b816c41dcc2733289d8cc8e36
ChavanPriya97 <priya971110@gmail.com>
2023-01-08 16:37:03 +0530

    done

---
+++ project/bookmanagement_frontend/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import UserRegistration from "./UserRegistration";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+     <UserRegistration />
+  </React.StrictMode>
+);
